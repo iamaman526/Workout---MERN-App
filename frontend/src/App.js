39060c69f6f4a6e57578d674615eb58ab809c8f2
iamaman526 <iamaman526@gmail.com>
2023-07-28 18:53:16 +0530

    added-footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // pages & components
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
             <Route path="/" element={<Home />} />
           </Routes>
         </div>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,12 +3,11 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <header>
-      <div className="container">
+      <div className="container bg-green">
         <Link to="/">
           <h1>Workout Buddy</h1>
         </Link>
       </div>
-      ;
     </header>
   );
 };
